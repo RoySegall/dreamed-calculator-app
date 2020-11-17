36a131268b5337876e33780de3a749f631f1007e
RoySegall <roy@segall.io>
2020-11-17 07:01:47 +0200

    Moving to a unit function

--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 
+from calculator.utils import concat_numbers
+
 
 def calculator_plus(request, first_number, second_number):
-	return JsonResponse({"results": first_number + second_number})
+	return JsonResponse({"results": concat_numbers(first_number, second_number)})
