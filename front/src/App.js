5f6f43c6e00b5c6643d63af249b898f3429bc70c
RoySegall <roy@segall.io>
2020-11-17 20:50:40 +0200

    Done with the look and feel

--- front/src/App.js
+++ front/src/App.js
@@ -1,37 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.scss';
 
+const handleNumberClick = (setCurrentNumber, currentNumber, selectedNumber) => {
+    if (currentNumber === 0) {
+        setCurrentNumber(selectedNumber);
+        return;
+    }
+
+    setCurrentNumber(String(currentNumber) + String(selectedNumber));
+};
+
+const NumberSection = ({numbers, sectionName, setCurrentNumber, currentNumber}) => <section className={sectionName}>
+    {numbers.map((number, index) =>
+        <button
+            key={number}
+            className={index === 1 ? 'middle-button' : ''}
+            onClick={() => {handleNumberClick(setCurrentNumber, currentNumber, number)}}
+        >{number}</button>
+    )}
+</section>
+
 function App() {
+    const [currentNumber, setCurrentNumber] = useState(0);
+    const [secondNumber, setSecondNumber] = useState(null);
+
     return (
         <div className="App">
             <div className="wrapper">
-                <section className="results">0</section>
+                <section className="results"><div className="first-number">{secondNumber}</div> {currentNumber}</section>
                 <section className="buttons">
                     <section className="numbers">
-                        <section className="upper">
-                            <button>7</button>
-                            <button className="middle-button">8</button>
-                            <button>9</button>
-                        </section>
-
-                        <section className="middle">
-                            <button>4</button>
-                            <button className="middle-button">5</button>
-                            <button>6</button>
-                        </section>
-
-                        <section className="bottom">
-                            <button>1</button>
-                            <button className="middle-button">2</button>
-                            <button>3</button>
-                        </section>
+                        <NumberSection numbers={[7, 8, 9]} sectionName="upper" setCurrentNumber={setCurrentNumber} currentNumber={currentNumber}/>
+                        <NumberSection numbers={[4, 5, 6]} sectionName="middle" setCurrentNumber={setCurrentNumber} currentNumber={currentNumber}/>
+                        <NumberSection numbers={[1, 2, 3]} sectionName="bottom" setCurrentNumber={setCurrentNumber} currentNumber={currentNumber}/>
                         <section className="ground-zero">
-                            <button>0</button>
+                            <button onClick={() => handleNumberClick(setCurrentNumber, currentNumber, 0)}>0</button>
                         </section>
                     </section>
 
                     <section className="actions">
-                        <button>+</button>
+                        <button className={`plus ${secondNumber ? 'disabled' : ''}`} onClick={() => {
+
+                            if (currentNumber === 0) {
+                                return;
+                            }
+
+                            setSecondNumber(currentNumber);
+                            setCurrentNumber(0);
+                        }} disabled={secondNumber}>+</button>
+                        <button className={`equals ${!secondNumber ? 'disabled' : ''}`} disabled={!secondNumber}>=</button>
                     </section>
                 </section>
             </div>
