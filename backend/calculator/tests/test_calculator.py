7262b1f1f0b672320e84630ccde68f24c3464758
roy segall <roy.segall@dreamed-diabetes.com>
2020-11-25 08:48:23 +0200

    Mocking the results

--- backend/calculator/tests/test_calculator.py
+++ backend/calculator/tests/test_calculator.py
@@ -1,16 +1,12 @@
+from unittest.mock import patch
+
 from django.test import TestCase, Client
+from calculator.utils import concat_numbers
 
 
 class TestCalculator(TestCase):
 
-	def _calculate_plus(self, client, first_number, second_number, expected):
-		response = client.get(f'/calculator/plus/{first_number}/{second_number}')
-		results = response.json().get('results')
-		self.assertEquals(results, expected)
-
 	def test_concat_numbers(self):
-		client = Client()
-
 		expected_results = []
 		for first_number in range(10):
 			for second_number in range(10):
@@ -21,11 +17,25 @@ class TestCalculator(TestCase):
 				})
 
 		for expected_result in expected_results:
-			self._calculate_plus(
-				client=client,
-				first_number=expected_result.get('first_number'),
-				second_number=expected_result.get('second_number'),
-				expected=expected_result.get('expected')
+			results = concat_numbers(
+				expected_result.get('first_number'),
+				expected_result.get('second_number')
 			)
+			self.assertEquals(results, expected_result.get('expected'))
 
+	def test_concat_numbers_view(self):
+		client = Client()
+		response = client.get(f'/calculator/plus/1/2')
+		results = response.json().get('results')
+		self.assertEquals(results, 3)
 
+	@patch('calculator.views.concat_numbers')
+	def test_unit_concat_numbers_view(self, concat_numbers_mock):
+		concat_numbers_mock.return_value = 3
+		client = Client()
+
+		response = client.get(f'/calculator/plus/1/2')
+		concat_numbers_mock.assert_called_with(1, 2)
+
+		results = response.json().get('results')
+		self.assertEquals(results, 3)
